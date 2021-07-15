330ce5c47cec841aa151d5e78f8fa532391aac0e
Mathias Buus <mathiasbuus@gmail.com>
2021-07-15 18:09:54 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = safetyCatch
+
+function isActuallyUncaught (err) {
+  return err instanceof TypeError ||
+    err instanceof SyntaxError ||
+    err instanceof ReferenceError ||
+    err instanceof EvalError ||
+    err instanceof RangeError ||
+    err instanceof URIError ||
+    err.code === 'ERR_ASSERTION'
+}
+
+function throwErrorNT (err) {
+  queueMicrotask(() => { throw err })
+}
+
+function safetyCatch (err) {
+  if (isActuallyUncaught(err)) {
+    throwErrorNT(err)
+    throw err
+  }
+}

fc96c8ef87fddc9507276b7378145cbcc44adb4a
Christophe Diederichs <chm-diederichs@protonmail.com>
2025-06-27 09:11:36 +0100

    check assertion name instead of code

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function isActuallyUncaught (err) {
     err instanceof EvalError ||
     err instanceof RangeError ||
     err instanceof URIError ||
-    err.code === 'ERR_ASSERTION'
+    err.name === 'AssertionError'
 }
 
 function throwErrorNT (err) {
